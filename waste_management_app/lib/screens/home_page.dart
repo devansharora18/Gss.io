f1ac0f752356200c1caf4e63ce969c97c67d9249
Devansh Arora <dev.marvel.avengers@gmail.com>
2022-10-04 15:15:12 +0530

    commit

---
+++ waste_management_app/lib/screens/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Center(child: Text('Home Page'))),
+    );
+  }
+}
