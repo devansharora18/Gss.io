fa57b53405dee0d816c5626eeedcd4c33bc0923b
Devansh Arora <dev.marvel.avengers@gmail.com>
2022-10-05 02:34:47 +0530

    commit

--- waste_management_app/lib/logic/snack_bar.dart
+++ waste_management_app/lib/logic/snack_bar.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
 showSnackBar(String content, BuildContext context) {
-  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(content)));
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    behavior: SnackBarBehavior.floating,
+    margin: EdgeInsets.only(
+        bottom: MediaQuery.of(context).size.height - 110, right: 20, left: 20),
+    content: Text(content),
+    backgroundColor: Colors.red,
+  ));
 }
