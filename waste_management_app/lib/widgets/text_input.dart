59e78804654431fcc0757b1f591a844596b13c26
Devansh Arora <dev.marvel.avengers@gmail.com>
2022-10-04 23:08:06 +0530

    commit

---
+++ waste_management_app/lib/widgets/text_input.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class TextInput extends StatelessWidget {
+  final TextEditingController textEditingController;
+  final bool isPass;
+  final String hintText;
+  final TextInputType textInputType;
+  const TextInput(
+      {super.key,
+      required this.textEditingController,
+      required this.hintText,
+      this.isPass = false,
+      required this.textInputType});
+
+  @override
+  Widget build(BuildContext context) {
+    final border =
+        OutlineInputBorder(borderSide: Divider.createBorderSide(context));
+    return TextField(
+      controller: textEditingController,
+      decoration: InputDecoration(
+        hintText: hintText,
+        border: border,
+        focusedBorder: border,
+        enabledBorder: border,
+        filled: true,
+        contentPadding: const EdgeInsets.all(8),
+      ),
+      keyboardType: textInputType,
+      obscureText: isPass,
+    );
+  }
+}
