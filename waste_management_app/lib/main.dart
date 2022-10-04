9eadf453e9c9e899b03b0746628e767e5a4ab382
Devansh Arora <dev.marvel.avengers@gmail.com>
2022-10-04 16:06:18 +0530

    commit

--- waste_management_app/lib/main.dart
+++ waste_management_app/lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:waste_management_app/screens/home_page.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
