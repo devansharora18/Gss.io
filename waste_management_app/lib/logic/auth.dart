9fc403e721dfdb9eede178c7186323871b589ce8
Devansh Arora <dev.marvel.avengers@gmail.com>
2022-10-05 00:13:15 +0530

    commit

--- waste_management_app/lib/logic/auth.dart
+++ waste_management_app/lib/logic/auth.dart
@@ -1,8 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Future<String> register({
     required String email,
@@ -11,7 +14,13 @@ class AuthMethods {
     String res = "Some error occured";
     try {
       if (email.isNotEmpty || password.isNotEmpty) {
-        UserCredential user = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+        UserCredential user = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+
+        _firestore.collection('users').doc(user.user!.uid).set({
+          'email': email,
+          'uid': user.user!.uid,
+        });
       }
     } catch (e) {
       res = e.toString();
