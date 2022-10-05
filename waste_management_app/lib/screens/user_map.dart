b8044031828cce80595ae5aed613c590b046d33b
Devansh Arora <dev.marvel.avengers@gmail.com>
2022-10-05 22:03:32 +0530

    commit

--- waste_management_app/lib/screens/user_map.dart
+++ waste_management_app/lib/screens/user_map.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import "package:latlong2/latlong.dart";
 
 class UserMap extends StatefulWidget {
   const UserMap({super.key});
@@ -17,6 +19,24 @@ class _UserMapState extends State<UserMap> {
             elevation: 0,
             backgroundColor: const Color.fromRGBO(30, 30, 46, 0.7),
             title: const Center(child: Text('Location     '))),
+      ),
+      body: FlutterMap(
+        options: MapOptions(
+          center: LatLng(51.509364, -0.128928),
+          zoom: 9.2,
+        ),
+        nonRotatedChildren: [
+          AttributionWidget.defaultWidget(
+            source: 'OpenStreetMap contributors',
+            onSourceTapped: null,
+          ),
+        ],
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'com.example.app',
+          ),
+        ],
       ),
     );
   }
