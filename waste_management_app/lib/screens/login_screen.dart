2adf2892a1d1d50d08445a9c6d1f430a942af1b1
Devansh Arora <dev.marvel.avengers@gmail.com>
2022-10-05 11:12:55 +0530

    commit

--- waste_management_app/lib/screens/login_screen.dart
+++ waste_management_app/lib/screens/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:waste_management_app/logic/auth.dart';
+import 'package:waste_management_app/logic/snack_bar.dart';
 import 'package:waste_management_app/widgets/text_input.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -12,12 +14,29 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _email = TextEditingController();
   final TextEditingController _password = TextEditingController();
+  bool _isLoading = false;
 
   @override
   void dispose() {
     _email.dispose();
     _password.dispose();
     super.dispose();
+  }
+
+  void loginUser() async {
+    setState(() {
+      _isLoading = true;
+    });
+    String res =
+        await AuthMethods().logIn(email: _email.text, password: _password.text);
+    setState(() {
+      _isLoading = false;
+    });
+    if (res == 'success') {
+
+    } else {
+      showSnackBar(res, context);
+    }
   }
 
   @override
@@ -70,7 +89,7 @@ class _LoginScreenState extends State<LoginScreen> {
             style: ElevatedButton.styleFrom(
               backgroundColor: Colors.green,
             ),
-            onPressed: () {},
+            onPressed: () => loginUser(),
             child: const Text('Login'),
           ),
           const Spacer(),
