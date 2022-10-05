cfa656549ce7884b5d645315fa38e32cdb2fac4f
Devansh Arora <dev.marvel.avengers@gmail.com>
2022-10-05 23:53:59 +0530

    commit

--- waste_management_app/lib/logic/firestore.dart
+++ waste_management_app/lib/logic/firestore.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:waste_management_app/logic/storage.dart';
+import 'package:waste_management_app/models/image.dart';
 
 class FirestoreMethods {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
@@ -8,11 +9,20 @@ class FirestoreMethods {
   Future<String> uploadImage(
     List cords,
     Uint8List file,
+    String uid,
+    String email,
   ) async {
     String res = 'Some error occured';
 
     try {
-      String photoUrl = await Storage().uploadImage('images', file);
+      String imageUrl = await Storage().uploadImage('images', file);
+
+      ImageModel image = ImageModel(
+          email: email,
+          uid: uid,
+          cords: cords,
+          imageId: '',
+          imageUrl: imageUrl);
     } catch (e) {}
   }
 }
