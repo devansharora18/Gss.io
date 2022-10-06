7445acf5f0f41186a122c02070ecb2abb6ae234d
Devansh Arora <dev.marvel.avengers@gmail.com>
2022-10-06 20:45:30 +0530

    commit

--- waste_management_app/lib/screens/admin_ui.dart
+++ waste_management_app/lib/screens/admin_ui.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:waste_management_app/widgets/frame.dart';
 
@@ -15,7 +16,21 @@ class AdminUI extends StatelessWidget {
             backgroundColor: const Color.fromRGBO(30, 30, 46, 0.7),
             title: const Text('Administrator')),
       ),
-      body: Frame(),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('images').snapshots(),
+        builder: (context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) => Frame(),
+          );
+        },
+      ),
     );
   }
 }
