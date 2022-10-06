46d432b7c2e41f8fac9c331a922a39321b2d29ac
Devansh Arora <dev.marvel.avengers@gmail.com>
2022-10-06 19:35:17 +0530

    commit

--- waste_management_app/lib/widgets/frame.dart
+++ waste_management_app/lib/widgets/frame.dart
@@ -6,7 +6,7 @@ class Frame extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(vertical: 13, horizontal: 20),
+      padding: const EdgeInsets.symmetric(vertical: 13, horizontal: 25),
       child: Column(children: [
         Container(
           //decoration: BoxDecoration(
@@ -15,7 +15,7 @@ class Frame extends StatelessWidget {
           //    color: Colors.red, // red as border color
           //  ),
           //),
-          padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16)
+          padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 0)
               .copyWith(right: 0),
           child: Row(
             children: [
@@ -25,6 +25,14 @@ class Frame extends StatelessWidget {
             ],
           ),
         ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.3,
+          width: double.infinity,
+          child: Image.network(
+            'https://images.unsplash.com/photo-1664747477807-566fbc875bdc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80',
+            fit: BoxFit.cover,
+          ),
+        )
       ]),
     );
   }
